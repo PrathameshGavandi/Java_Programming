1197f575e73aec65602cef11a753033929b7a8ab
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2026-02-15 14:20:28 +0530

    Add files via upload

---
+++ Classwork/program624.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class program624
+{
+    public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number : ");
+        int iNo = sobj.nextInt();
+
+        int iMask = 4;
+        int iResult = 0;
+
+
+        iResult = iNo & iMask;
+
+        if(iResult == iMask)
+        {
+            System.out.println("Third bit is ON");
+        }
+        else
+        {
+            System.out.println("Third bit is OFF");
+        }
+
+    }
+}
