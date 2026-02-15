e45cd309266890af548b3ac8011b3f2f5c83b6d5
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2026-02-15 14:19:35 +0530

    Add files via upload

---
+++ Classwork/program622.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class program622
+{
+    public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter a number : ");
+        int no = sobj.nextInt();
+        int iDigit = 0;
+        int iCount = 0;
+
+        while (no != 0) 
+        {
+            iDigit = no % 2;
+            
+            iCount = iCount + iDigit;
+
+            no = no /2;
+            
+        }
+
+        System.out.println("Number of 1's are : " + iCount);
+
+    }
+}
+
+/*
+    Decimal         Hexadecimal         Binary
+        0               0               0000
+        1               1               0001
+        2               2               0010
+        3               3               0011
+        4               4               0100
+        5               5               0101
+        6               6               0110
+        7               7               0111
+        8               8               1000
+        9               9               1001
+        10              a               1010
+        11              b               1011
+        12              c               1100
+        13              d               1101
+        14              e               1110
+        15              f               1111
+
+
+*/
