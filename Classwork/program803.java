f16853032742c825ea00d3b4ed13dd072a516421
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2026-04-23 18:38:14 +0530

    Add files via upload

---
+++ Classwork/program803.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class program803
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int size = 0;
+
+        System.out.println("Enter how many elemts you want");
+        size = sobj.nextInt();
+
+        long i = 0,First = 0,Second = 1,Next = 0;
+
+
+        for(i = 1, First = 0, Second = 1; i <= size; i++)
+        {
+            Next = First + Second;
+            System.out.print(First + " ");
+            Second = First;
+            First = Next;
+        }
+
+        sobj.close();
+    }
+}
