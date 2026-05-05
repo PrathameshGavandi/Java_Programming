f39ffee6a4fb2d0de59b1bf96e3c649a5294b930
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2026-05-05 20:30:25 +0530

    Add files via upload

---
+++ Classwork/program811.java
@@ -0,0 +1,86 @@
+import java.util.*;
+class program811
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        
+        System.out.println("Enter number of rows in theatre : ");
+        int R = sobj.nextInt();
+
+        System.out.println("Enter number of columns : ");
+        int C = sobj.nextInt();
+
+        if(R <= 0 || C <= 0)
+        {
+            System.out.println("Invalid input");
+            return;
+        }
+
+        int Seats[][] = new int[R][C];
+        int i = 0, j = 0;
+
+        System.out.println("Enter booking details (0/1) : ");
+
+        for(i = 0; i < R; i++)
+        {
+            System.out.println("Enter details of row : " + (i + 1));
+            for(j = 0; j < C; j++)
+            {
+                Seats[i][j] = sobj.nextInt();
+
+                if(Seats[i][j] != 0 && Seats[i][j] != 1)
+                {
+                    System.out.println("Invalid input");
+                    return;
+                }
+            }
+        }
+
+        int TotalBooked = 0;
+        int MaxBookedInRow = 0;
+        int RowWithMax = 0;
+        int RowBooked = 0;
+        boolean FullRowExists = false;
+
+        for(i = 0; i < R; i++)
+        {
+            RowBooked = 0;
+            for(j = 0; j < C; j++)
+            {
+                if(Seats[i][j] == 1)
+                {
+                    TotalBooked++;
+                }
+
+                RowBooked = RowBooked + Seats[i][j];
+            }
+
+            if(RowBooked > MaxBookedInRow)
+            {
+                MaxBookedInRow = RowBooked;
+                RowWithMax = i;
+            }
+
+            if(RowBooked == C)
+            {
+                FullRowExists = true;
+            }
+        }
+
+        System.out.println("Total Booked seats : " + TotalBooked);
+        System.out.println("Row with maximum bookings : " + (RowWithMax + 1));
+
+        if(FullRowExists == true)
+        {
+            System.out.println("Full row exists : Yes");
+        }
+        else
+        {
+            System.out.println("Full row exists : No");
+        }
+        
+
+        sobj.close();
+    }
+}
