c95e6d90b16db89583f1271b44c89f7d2e4673f0
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2026-06-10 14:24:25 +0530

    Add files via upload

---
+++ Classwork/program883.java
@@ -0,0 +1,26 @@
+/*
+    0   1   2   3   4   5   6   7   8   9   .....
+    a   b   c   d   e   f   g   h   i   j   .....
+    97  98  99  100 101 102 103 104 105 106 .....
+*/
+
+import java.util.*;
+
+class program883
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        ArrayList <Integer> aobj = new ArrayList<Integer>();
+
+        aobj.add(11);
+        aobj.add(21);
+        aobj.add(51);
+        aobj.add(101);
+        aobj.add(111);
+
+        System.out.println(aobj);
+
+    }
+}
