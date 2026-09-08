f24366a51f014e3096a852a593c55f7751bb0fe4
Prathamesh_Gavandi <115519891+PrathameshGavandi@users.noreply.github.com>
2026-09-08 22:41:20 +0530

    Add files via upload

--- Classwork/Reverse.java
+++ Classwork/Reverse.java
@@ -1,17 +1,53 @@
-class Reverse {
-    public String reverseString() {
-        String input = "Prathamesh";
-        String reversed = "";
+import java.util.*;
+import java.util.Scanner;
 
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i); // ✅ correct usage
+class demo
+{
+    public int[] Reverse(int Arr[],int iSize)
+    {
+        int iCnt = 0;
+        int iCount = 0;
+        int Brr[] = null;
+
+        Brr = new int[iSize];
+
+        for(iCnt = Arr.length - 1; iCnt >= 0; iCnt--)
+        {
+            Brr[iCount]  = Arr[iCnt];
+            iCount++;
         }
-        return reversed;
+        return Brr;
     }
+}
 
-    public static void main(String[] args) {
-        Reverse obj = new Reverse();
-        String output = obj.reverseString(); // ✅ method name matches
-        System.out.println(output);
+class Reverse
+{
+    public static void main(String A[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int Arr[];
+        int Brr[];
+        int iSize = 0;
+        int iCnt = 0;
+        
+        System.out.println("Enter How many elements you want :");
+        iSize = sc.nextInt();
+
+        Arr = new int[iSize];
+        System.out.println("Enter the elements :");
+        for(iCnt = 0; iCnt < iSize; iCnt++)
+        {
+            Arr[iCnt] = sc.nextInt();
+        }
+
+        demo dobj = new demo();
+        Brr = dobj.Reverse(Arr,iSize);
+
+        System.out.println("Reversed elements of existing array in new array Brr is :");
+        for(iCnt = 0; iCnt < iSize; iCnt++)
+        {
+            System.out.println("Element reversed one : "+Brr[iCnt]);
+        }
     }
 }
